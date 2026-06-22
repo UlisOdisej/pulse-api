b5d761de3eafac9b53c2a24efdcd0f3c0829fe7f
UlisOdisej <bsavkovic@gmail.com>
2026-06-22 14:12:19 +0200

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,9 +1,5 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
-);
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 export default async function handler(req, res) {
   try {
